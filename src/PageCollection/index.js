f0e3325594d30bc132b56d2ea4a1142f92fa41cc
junking <827652549@qq.com>
2020-03-07 23:08:43 +0800

    实现点击修改评分星星

--- src/PageCollection/index.js
+++ src/PageCollection/index.js
@@ -1,44 +1,8 @@
 import React, {Component} from 'react';
-import {Table, Rate} from 'antd';
+import {Table, Rate, Layout} from 'antd';
+import {List} from "immutable";
 
-
-const columns = [
-    {title: 'Id', dataIndex: 'id', key: 'id', sorter: (a, b) => a.id - b.id},
-    {title: '标题', dataIndex: 'title', key: 'title'},
-    {title: '链接', dataIndex: 'link', key: 'link'},
-    {
-        title: '类型', dataIndex: 'type', key: 'type', sorter: (a, b) => a.type.length - b.type.length,
-        filters: [
-            {
-                text: 'Web',
-                value: 'Web',
-            },
-            {
-                text: '网络',
-                value: '网络',
-            },
-            {
-                text: '算法',
-                value: '算法',
-            },
-            {
-                text: '生活',
-                value: '生活',
-            },
-
-        ],
-        filterMultiple: true,
-        onFilter: (value, record) => record.type.indexOf(value) === 0,
-    },
-    {title: '是否掌握', dataIndex: 'study', key: 'study', render: (text) => <Rate value={text}/>,sorter: (a, b) => a.study - b.study},
-    {title: '时间', dataIndex: 'time', key: 'time',sorter: (a, b) => a.time - b.time},
-    {
-        title: '操作',
-        dataIndex: '',
-        key: 'x',
-        render: () => <a>Delete</a>,
-    },
-];
+const {Header, Footer, Content} = Layout;
 
 const data = [
     {
@@ -68,26 +32,93 @@ const data = [
         link: 'http://www.baidu.com',
         type: '算法',
         study: 1,
-        time:Date(Date.now()),
+        time: Date(Date.now()),
         description: 'My name is John Brown, I am 32 years old, living in New York No. 1 Lake Park.',
     }
 ];
 
 class PageCollection extends Component {
 
+    constructor(props) {
+        super(props);
+        this.state = {data: data};
+
+
+        this.handleRatechange = this.handleRatechange.bind(this);
+    }
+
     render() {
+        const columns = [
+            {title: 'Id', dataIndex: 'id', key: 'id', sorter: (a, b) => a.id - b.id},
+            {title: '标题', dataIndex: 'title', key: 'title'},
+            {title: '链接', dataIndex: 'link', key: 'link'},
+            {
+                title: '类型', dataIndex: 'type', key: 'type', sorter: (a, b) => a.type.length - b.type.length,
+                filters: [
+                    {
+                        text: 'Web',
+                        value: 'Web',
+                    },
+                    {
+                        text: '网络',
+                        value: '网络',
+                    },
+                    {
+                        text: '算法',
+                        value: '算法',
+                    },
+                    {
+                        text: '生活',
+                        value: '生活',
+                    },
+
+                ],
+                filterMultiple: true,
+                onFilter: (value, record) => record.type.indexOf(value) === 0,
+            },
+            {
+                title: '掌握',
+                dataIndex: 'study',
+                key: 'study',
+                render: (text, record) => <Rate value={text}
+                                                onChange={(value) => this.handleRatechange(value, record.id)}/>,
+                sorter: (a, b) => a.study - b.study
+            },
+            {title: '时间', dataIndex: 'time', key: 'time', sorter: (a, b) => a.time - b.time},
+            {
+                title: '操作',
+                dataIndex: '',
+                key: 'x',
+                render: () => <a>Delete</a>,
+            },
+        ];
+
+
         return (
-            <Table
-                columns={columns}
-                expandable={{
-                    expandedRowRender: record => <p style={{margin: 0}}>{record.description}</p>,
-                    rowExpandable: record => record.name !== 'Not Expandable',
-                }}
-                dataSource={data}
-            />
+            <Layout>
+                <Header style={{backgroundColor:'lightgray'}}>Header</Header>
+                <Content>
+                    <Table
+                        columns={columns}
+                        expandable={{
+                            expandedRowRender: record => <p style={{margin: 0}}>{record.description}</p>,
+                            rowExpandable: record => record.name !== 'Not Expandable',
+                        }}
+                        dataSource={this.state.data}
+                    />
+                </Content>
+                {/*<Footer>Footer</Footer>*/}
+            </Layout>
+
         )
     }
 
+    handleRatechange = (value, id) => {
+        const newState = this.state;
+        const arr = List(newState.data).setIn([id - 1, 'study'], value).toArray();
+        this.setState({data: arr});
+    };
 }
+
 
 export default PageCollection;
