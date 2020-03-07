19d91e765ac00d840d43e3f04cac8057abdc65c8
junking <827652549@qq.com>
2020-03-07 14:05:40 +0800

    init

--- src/PageCollection/index.js
+++ src/PageCollection/index.js
@@ -1,12 +1,39 @@
-import React,{Component} from 'react';
-import { Table } from 'antd';
+import React, {Component} from 'react';
+import {Table, Rate} from 'antd';
+
 
 const columns = [
-    { title: 'Name', dataIndex: 'name', key: 'name' },
-    { title: 'Age', dataIndex: 'age', key: 'age' },
-    { title: 'Address', dataIndex: 'address', key: 'address' },
+    {title: 'Id', dataIndex: 'id', key: 'id', sorter: (a, b) => a.id - b.id},
+    {title: '标题', dataIndex: 'title', key: 'title'},
+    {title: '链接', dataIndex: 'link', key: 'link'},
     {
-        title: 'Action',
+        title: '类型', dataIndex: 'type', key: 'type', sorter: (a, b) => a.type.length - b.type.length,
+        filters: [
+            {
+                text: 'Web',
+                value: 'Web',
+            },
+            {
+                text: '网络',
+                value: '网络',
+            },
+            {
+                text: '算法',
+                value: '算法',
+            },
+            {
+                text: '生活',
+                value: '生活',
+            },
+
+        ],
+        filterMultiple: true,
+        onFilter: (value, record) => record.type.indexOf(value) === 0,
+    },
+    {title: '是否掌握', dataIndex: 'study', key: 'study', render: (text) => <Rate value={text}/>,sorter: (a, b) => a.study - b.study},
+    {title: '时间', dataIndex: 'time', key: 'time',sorter: (a, b) => a.time - b.time},
+    {
+        title: '操作',
         dataIndex: '',
         key: 'x',
         render: () => <a>Delete</a>,
@@ -16,42 +43,44 @@ const columns = [
 const data = [
     {
         key: 1,
-        name: 'John Brown',
-        age: 32,
-        address: 'New York No. 1 Lake Park',
+        id: 1,
+        title: 'canvas性能优化',
+        link: 'http://www.baidu.com',
+        type: 'Web',
+        study: 5,
+        time: Date(Date.now()),
         description: 'My name is John Brown, I am 32 years old, living in New York No. 1 Lake Park.',
     },
     {
         key: 2,
-        name: 'Jim Green',
-        age: 42,
-        address: 'London No. 1 Lake Park',
-        description: 'My name is Jim Green, I am 42 years old, living in London No. 1 Lake Park.',
+        id: 2,
+        title: '进阶阿里巴巴之路',
+        link: 'http://www.baidu.com',
+        type: '生活',
+        study: 3,
+        time: Date(Date.now()),
+        description: 'My name is John Brown, I am 32 years old, living in New York No. 1 Lake Park.',
     },
     {
         key: 3,
-        name: 'Not Expandable',
-        age: 29,
-        address: 'Jiangsu No. 1 Lake Park',
-        description: 'This not expandable',
-    },
-    {
-        key: 4,
-        name: 'Joe Black',
-        age: 32,
-        address: 'Sidney No. 1 Lake Park',
-        description: 'My name is Joe Black, I am 32 years old, living in Sidney No. 1 Lake Park.',
-    },
+        id: 3,
+        title: 'canvas教程',
+        link: 'http://www.baidu.com',
+        type: '算法',
+        study: 1,
+        time:Date(Date.now()),
+        description: 'My name is John Brown, I am 32 years old, living in New York No. 1 Lake Park.',
+    }
 ];
 
 class PageCollection extends Component {
 
-    render(){
-        return(
+    render() {
+        return (
             <Table
                 columns={columns}
                 expandable={{
-                    expandedRowRender: record => <p style={{ margin: 0 }}>{record.description}</p>,
+                    expandedRowRender: record => <p style={{margin: 0}}>{record.description}</p>,
                     rowExpandable: record => record.name !== 'Not Expandable',
                 }}
                 dataSource={data}
